de47d1da8e1f56e3f1453a3fde13d21111d01832
vivianne <viviannjoroge91@gmail.com>
2023-02-13 09:13:57 +0300

    completed assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,13 @@
 ## code your solution here. 
+class Cat
+    attr_accessor :name
+end
+class Cat
+    public
+    def meow
+      puts "meow!"
+    end
+  end
+cat = Cat.new
+cat.meow
+ #Output: "meow!"
